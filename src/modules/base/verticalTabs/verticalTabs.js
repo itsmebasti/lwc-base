256272222f8464904cf0b3c6349c599ca8d7a735
Sebastian Schwarz <12232063+itsmebasti@users.noreply.github.com>
2022-10-07 10:44:35 +0200

    made tabs responsive

--- src/modules/base/verticalTabs/verticalTabs.js
+++ src/modules/base/verticalTabs/verticalTabs.js
@@ -3,11 +3,15 @@ import { LightningElement, track } from 'lwc';
 export default class VerticalTabs extends LightningElement {
     _selected;
     @track tabs = [];
+    isDesktop;
 
     connectedCallback() {
         this.selected = window.location.hash?.substring(1);
         window.onpopstate = ({ state }) => this.selected = state?.page;
+        
+        window.matchMedia(`(max-width: 1000px)`).addEventListener("change", (evt) => this.isDesktop = !window.matchMedia('(max-width: 1000px)').matches)
     }
+    
 
     initialize(evt) {
         this.tabs = evt.target.assignedNodes();
@@ -44,8 +48,32 @@ export default class VerticalTabs extends LightningElement {
                 label: tab.label,
                 icon: tab.icon,
                 badge: tab.badge,
-                classes: 'slds-vertical-tabs__nav-item ' + ((tab.id === this.selected) ? 'slds-is-active' : '')
+                classes: (this.isDesktop ? 'slds-vertical-tabs__nav-item ' : 'slds-tabs_scoped__item ') + ((tab.id === this.selected) ? 'slds-is-active' : '')
             };
         });
+    }
+    
+    get divClass() {
+        return this.isDesktop ? 'slds-vertical-tabs slds-template__container' : 'slds-tabs_scoped slds-template__container';
+    }
+    
+    get ulClass() {
+        return this.isDesktop ? 'slds-vertical-tabs__nav' : 'slds-tabs_scoped__nav';
+    }
+    
+    get linkClass() {
+        return this.isDesktop ? 'slds-vertical-tabs__link' : 'slds-tabs_scoped__link';
+    }
+    
+    get contentClass() {
+        return this.isDesktop ? 'slds-vertical-tabs__content slds-scrollable' : 'slds-tabs_scoped__content slds-scrollable';
+    }
+    
+    get badgeClass() {
+        return this.isDesktop ? 'slds-vertical-tabs__right-icon slds-badge slds-badge_inverse' : '';
+    }
+    
+    get iconClass() {
+        return this.isDesktop ? 'slds-vertical-tabs__left-icon' : '';
     }
 }
