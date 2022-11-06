7618ea03a8f75c15777184155f4ab9c37501e2fb
Sebastian Schwarz <12232063+itsmebasti@users.noreply.github.com>
2022-11-07 00:04:37 +0100

    simplified select rendering workaround

--- src/modules/base/select/select.js
+++ src/modules/base/select/select.js
@@ -2,27 +2,28 @@ import { LightningElement, api } from 'lwc';
 
 export default class Select extends LightningElement {
     @api label;
-    @api value;
-    _options;
+    @api options;
+    _value;
 
     bubble(evt) {
         this.dispatchEvent(new CustomEvent('change', {detail: evt.target.value}))
     }
-
-    @api set selectedIndex(index) {
-        this.template.querySelector('select').selectedIndex = index;
+    
+    @api set value(value) {
+        this._value = value;
+        
+        // Note: solve rendering issues https://salesforce.stackexchange.com/q/369236/2513
+        if(this.template.querySelector('select')) {
+            this.template.querySelector('select').selectedIndex = this.options.indexOf(value);
+        }
+    }
+    
+    get value() {
+        return this._value;
     }
 
-    get selectedIndex() {
-        return this.template.querySelector('select').selectedIndex;
-    }
-
-    @api set options(value) {
-        this._options = value;
-    }
-
-    get options() {
-        return this._options?.map((option) => ({
+    get selectOptions() {
+        return this.options?.map((option) => ({
             value: option,
             selected: (option === this.value)
         })) ?? [];
